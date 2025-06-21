94109450e7615990b708c30df5eead0cc9992112
mgupta27 <mihir_gupta@rocketmail.com>
2025-06-21 16:08:08 -0700

    Added simple image viewer for testing

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@
 ARGUS 3D mapping reconstruction.
 """
 
+import cv2
 import time
 import multiprocessing as mp
 
+from modules.image_data import ImageData
 from modules.image_collector.image_receiver_controller import ImageReceiverController 
 
 
@@ -25,7 +27,32 @@ def main() -> int:
 
     try:
         image_receiver_controller.start_workers()
-        time.sleep(20)
+
+        while True:
+            image_data = image_to_mapping_queue.get()
+
+            if image_data is None:
+                continue
+
+            assert isinstance(image_data, ImageData)
+
+            cv2.putText(
+                image_data.image,
+                f"{image_data.timestamp:.3f}",
+                (10, 20),  # Top-left corner
+                cv2.FONT_HERSHEY_SIMPLEX,  # Font type
+                0.5,  # Font size
+                (255, 0, 255),  # Fuschia
+                1,  # Line thickness
+            )
+
+            cv2.imshow(f"Image display", image_data.image)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            if cv2.getWindowProperty("Image display", cv2.WND_PROP_VISIBLE) < 1:
+                break
     finally:
         image_receiver_controller.stop_workers()
 
